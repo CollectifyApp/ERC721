6d6818dc9fb001b64b35752f2843430e1bc8c621
Hg <hging3@gmail.com>
2022-07-27 18:54:05 +0800

    Add first ERC721TOKEN file for mint

---
+++ constructor.js
@@ -0,0 +1,50 @@
+module.exports = [
+  "Soulbound",
+  "SOUL",
+  0,
+  10,
+  10,
+  "https://launchpad.collectify.dev/main/metadata/DP9X7Z659/",
+  0,
+  [
+      8,
+      "(UTC+08:00) Beijing, Chongqing, Hong Kong, Urumqi"
+  ],
+  [
+      1658419200,
+      1973831099
+  ],
+  [
+      1658419200,
+      1973831099
+  ]
+];
+
+
+
+//     "royalties": 0,
+//     "totalSupply": {
+//         "type": "BigNumber",
+//         "hex": "0x00"
+//     },
+//     "privateTime": [
+//         {
+//             "type": "BigNumber",
+//             "hex": "0x62d97800"
+//         },
+//         {
+//             "type": "BigNumber",
+//             "hex": "0x75a64a2c"
+//         }
+//     ],
+//     "publicTime": [
+//         {
+//             "type": "BigNumber",
+//             "hex": "0x62d97800"
+//         },
+//         {
+//             "type": "BigNumber",
+//             "hex": "0x75a64a2c"
+//         }
+//     ]
+// }
